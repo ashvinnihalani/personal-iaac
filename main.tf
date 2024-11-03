3221ef640bb08a4d064849529e5ed426e334cbcc
Ashvin Nihalani <ashvin.nihalani@gmail.com>
2024-11-02 23:56:11 -0700

    Adding AWS Provider

--- main.tf
+++ main.tf
@@ -11,15 +11,33 @@ terraform {
       source  = "digitalocean/digitalocean"
       version = "~> 2.0"
     }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
   }
 }
-
 
 variable "DO_PAT" {
   type    = string
   default = ""
 }
 
+variable "AWS_ACCESS_KEY_ID" {
+  type    = string
+  default = ""
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  type    = string
+  default = ""
+}
 provider "digitalocean" {
   token = var.DO_PAT
+}
+
+provider "aws" {
+  region     = "us-west-2"
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
 }
